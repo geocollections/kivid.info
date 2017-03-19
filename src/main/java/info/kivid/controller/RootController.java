9513ba3e913f42ae887c46e58fa57e53c2e5ea39
Liis Harjo <liis@snowhound.eu>
2017-03-19 15:21:55 +0200

    Translation system #3

---
+++ src/main/java/info/kivid/controller/RootController.java
@@ -0,0 +1,13 @@
+package info.kivid.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RootController {
+
+    @RequestMapping("/")
+    public String rocks() {
+        return "front_page";
+    }
+}
