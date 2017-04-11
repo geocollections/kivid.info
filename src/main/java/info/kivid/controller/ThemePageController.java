a5e5a947925653d78a582ef13e17b7b60b0190fb
Liis Harjo <liis@snowhound.eu>
2017-04-11 11:55:00 +0300

    Refactored html to reduce code reuse #13

--- src/main/java/info/kivid/controller/ThemePageController.java
+++ src/main/java/info/kivid/controller/ThemePageController.java
@@ -4,10 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MineralsController {
+public class ThemePageController {
+
+    @RequestMapping("/rocks")
+    public String rocks() {
+        return "rocks";
+    }
 
     @RequestMapping("/minerals")
     public String minerals() {
         return "minerals";
+    }
+
+    @RequestMapping("/resources")
+    public String resources() {
+        return "/resources";
     }
 }
